48ae59a72e93e5ede7e52c1cb85c654375bedfe6
dharsanb <dharsanborn98@gmail.com>
2024-06-18 13:28:22 +0530

    feat(semver): add support for semantic version as input

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import os from 'os'
 import * as core from '@actions/core'
 import * as tc from '@actions/tool-cache'
+import { Octokit } from '@octokit/rest'
+import * as semver from 'semver'
+import * as process from 'node:process'
 
 // arch in [arm, x32, x64...] (https://nodejs.org/api/os.html#os_os_arch)
 // return value in [amd64, 386, arm]
@@ -29,27 +32,52 @@ function mapPlatform(platform: string): string {
   }
 }
 
-function getDownloadUrl(version: string): string {
+async function getDownloadUrl(version: string, token: string): Promise<string> {
   const platform = mapPlatform(os.platform())
   const arch = mapArch(os.arch())
-
   const extension = platform === 'windows' ? 'zip' : 'tar.gz'
 
   if (version === '0.11.0') {
     return `https://storage.googleapis.com/humctl-releases/v${version}/cli_${version}_${platform}_${arch}.${extension}`
-  } else {
-    return `https://github.com/humanitec/cli/releases/download/v${version}/cli_${version}_${platform}_${arch}.${extension}`
   }
-}
+  const oktokit = new Octokit({ auth: token })
 
+  const response = await oktokit.rest.repos.listReleases({
+    owner: 'humanitec',
+    repo: 'cli'
+  })
+  const releases = response.data
+  // Get all tags as an array
+  const tags = releases.map(release => {
+    const semverVersion = semver.parse(release.tag_name)
+    if (semverVersion === null) {
+      throw new Error(`Failed to parse version from tag: ${release.tag_name}`)
+    }
+    return semverVersion.version
+  })
+
+  let downloadVersion: string | null = null
+  if (version !== 'latest') {
+    // Get the maximum version that satisfies the condition
+    downloadVersion = semver.maxSatisfying(tags, version)
+    if (downloadVersion == null) {
+      throw new Error('Requested version not found in releases')
+    }
+  } else {
+    downloadVersion = tags[0]
+  }
+
+  return `https://github.com/humanitec/cli/releases/download/v${downloadVersion}/cli_${downloadVersion}_${platform}_${arch}.${extension}`
+}
 /**
  * The main function for the action.
  * @returns {Promise<void>} Resolves when the action is complete.
  */
 export async function run(): Promise<void> {
   try {
-    const version = core.getInput('version')
-    const url = getDownloadUrl(version)
+    const version: string = core.getInput('version')
+    const token: string = process.env['GITHUB_TOKEN'] ?? ''
+    const url: string = await getDownloadUrl(version, token)
 
     core.info(`Downloading: ${url}`)
     const pathToTarball = await tc.downloadTool(url)
